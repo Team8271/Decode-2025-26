8ce04db602c06fd40cd967eb335df59ccf748c21
Team8271 <31601375+Team8271@users.noreply.github.com>
2025-10-28 15:27:01 -0600

    style: renamed TeleOp driver station entry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/TeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/TeleOpMode.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name = "DevOp")
+@TeleOp(name = "TeleOp")
 public class TeleOpMode extends LinearOpMode {
     @Override
     public void runOpMode() {
